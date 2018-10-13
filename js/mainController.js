a388b0ec9e5c90cf6cd781f0ebcaef5cd194a0ba
Pamela Ocampo <pamela.ocampo@gmail.com>
2018-10-12 20:08:47 -0700

    Extract features into constant

--- js/mainController.js
+++ js/mainController.js
@@ -13,111 +13,26 @@ angular
     '$rootScope',
     'FirebaseService',
     'ModalService',
-    function($scope, $filter, $window, utils, auth, $rootScope, firebaseService, modalService) {
+    'Features',
+    function(
+      $scope,
+      $filter,
+      $window,
+      utils,
+      auth,
+      $rootScope,
+      firebaseService,
+      modalService,
+      features
+    ) {
       $scope.loading = true;
       $scope.messageTypes = utils.messageTypes;
       $scope.utils = utils;
       $scope.newBoard = {
         name: '',
-        text_editing_is_private: true,
+        text_editing_is_private: true
       };
-      $scope.features = [
-        {
-          name: 'Unlimited public boards',
-          free: 'X',
-          premium: 'X',
-        },
-        {
-          name: 'Create and merge cards',
-          free: 'X',
-          premium: 'X',
-        },
-        {
-          name: 'Sort and search cards',
-          free: 'X',
-          premium: 'X',
-        },
-        {
-          name: 'Export PDF and import/export CSV',
-          free: 'X',
-          premium: 'X',
-        },
-        {
-          name: 'Edit or delete columns',
-          free: 'Everyone',
-          premium: 'Only board creator',
-        },
-        {
-          name: 'Edit or delete cards',
-          free: 'Everyone',
-          premium: 'Only card creator and admin',
-        },
-        {
-          name: 'Maximum columns',
-          free: '6',
-          premium: 'Unlimited',
-        },
-        {
-          name: 'History and search of past retrospectives',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Manage multiple teams',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Unlimited team boards',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Invite people to retrospectives',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Order cards inside a column',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Change color of a column',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Login using Google and Email',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Comments on cards',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: "Display card's author",
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Pre-defined retrospective formats',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Clone a board',
-          free: '',
-          premium: 'X',
-        },
-        {
-          name: 'Slack integration',
-          free: '',
-          premium: 'X',
-        },
-      ];
+      $scope.features = features;
       $scope.userId = $window.location.hash.substring(1) || '';
       $scope.searchParams = {};
       $window.location.search
@@ -131,19 +46,22 @@ angular
       $scope.selectedType = 1;
       $scope.import = {
         data: [],
-        mapping: [],
+        mapping: []
       };
 
       $scope.droppedEvent = function(dragEl, dropEl) {
         var drag = $('#' + dragEl);
         var drop = $('#' + dropEl);
-        var dragMessageRef = firebaseService.getMessageRef($scope.userId, drag.attr('messageId'));
+        var dragMessageRef = firebaseService.getMessageRef(
+          $scope.userId,
+          drag.attr('messageId')
+        );
 
         dragMessageRef.once('value', function() {
           dragMessageRef.update({
             type: {
-              id: drop.data('column-id'),
-            },
+              id: drop.data('column-id')
+            }
           });
         });
       };
@@ -195,7 +113,7 @@ angular
 
       $scope.updatePrivateWritingToggle = function(privateWritingOn) {
         $scope.boardRef.update({
-          text_editing_is_private: privateWritingOn,
+          text_editing_is_private: privateWritingOn
         });
       };
 
@@ -205,7 +123,9 @@ angular
       };
 
       $scope.getSortFields = function() {
-        return $scope.sortField === 'votes' ? ['-votes', 'date_created'] : 'date_created';
+        return $scope.sortField === 'votes'
+          ? ['-votes', 'date_created']
+          : 'date_created';
       };
 
       $scope.saveMessage = function(message) {
@@ -214,7 +134,11 @@ angular
       };
 
       function redirectToBoard() {
-        window.location.href = window.location.origin + window.location.pathname + '#' + $scope.userId;
+        window.location.href =
+          window.location.origin +
+          window.location.pathname +
+          '#' +
+          $scope.userId;
       }
 
       $scope.isBoardNameInvalid = function() {
@@ -239,7 +163,7 @@ angular
               columns: $scope.messageTypes,
               user_id: userData.uid,
               max_votes: $scope.newBoard.max_votes || 6,
-              text_editing_is_private: $scope.newBoard.text_editing_is_private,
+              text_editing_is_private: $scope.newBoard.text_editing_is_private
             },
             function(error) {
               if (error) {
@@ -258,13 +182,13 @@ angular
 
       $scope.changeBoardContext = function() {
         $scope.boardRef.update({
-          boardContext: $scope.boardContext,
+          boardContext: $scope.boardContext
         });
       };
 
       $scope.changeBoardName = function(newBoardName) {
         $scope.boardRef.update({
-          boardId: newBoardName,
+          boardId: newBoardName
         });
 
         modalService.closeAll();
@@ -272,7 +196,11 @@ angular
 
       $scope.updateSortOrder = function() {
         var updatedFilter =
-          $window.location.origin + $window.location.pathname + '?sort=' + $scope.sortField + $window.location.hash;
+          $window.location.origin +
+          $window.location.pathname +
+          '?sort=' +
+          $scope.sortField +
+          $window.location.hash;
         $window.history.pushState({ path: updatedFilter }, '', updatedFilter);
       };
 
@@ -283,7 +211,7 @@ angular
 
         $scope.board.columns.push({
           value: name,
-          id: utils.getNextId($scope.board),
+          id: utils.getNextId($scope.board)
         });
 
         var boardColumns = firebaseService.getBoardColumns($scope.userId);
@@ -341,11 +269,11 @@ angular
             creating: true,
             user_id: $scope.userUid,
             type: {
-              id: type.id,
+              id: type.id
             },
             date: firebaseService.getServerTimestamp(),
             date_created: firebaseService.getServerTimestamp(),
-            votes: 0,
+            votes: 0
           })
           .then(addMessageCallback);
       };
@@ -401,5 +329,5 @@ angular
         $scope.userId = $window.location.hash.substring(1) || '';
         auth.logUser($scope.userId, getBoardAndMessages);
       });
-    },
+    }
   ]);
