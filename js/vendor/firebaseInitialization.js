17931a286217229ee79f58f5d5c62a8772fcd0ea
Ronald Müller <ronald_mueller@gmx.de>
2020-03-17 22:09:18 +0100

    Update firebaseInitialization.js

--- js/vendor/firebaseInitialization.js
+++ js/vendor/firebaseInitialization.js
@@ -1,10 +1,10 @@
 var config = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGE_ID"
+  apiKey: "AIzaSyAbQsfE6Et5KDIJ6TALRKbUYJmFD0uGVkI",
+  authDomain: "quip-1bc04.firebaseapp.com",
+  databaseURL: "https://quip-1bc04.firebaseio.com",
+  projectId: "quip-1bc04",
+  storageBucket: "quip-1bc04.appspot.com",
+  messagingSenderId: "QUIP"
 };
 
 firebase.initializeApp(config);
