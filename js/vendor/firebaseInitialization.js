7480a22a873280c50b1ab856b19dc73190f031b5
Ronald Müller <ronald_mueller@gmx.de>
2020-03-17 22:17:41 +0100

    Update firebaseInitialization.js

--- js/vendor/firebaseInitialization.js
+++ js/vendor/firebaseInitialization.js
@@ -4,7 +4,7 @@ var config = {
   databaseURL: "https://quip-1bc04.firebaseio.com",
   projectId: "quip-1bc04",
   storageBucket: "quip-1bc04.appspot.com",
-  messagingSenderId: "QUIP"
+  messagingSenderId: "398140502966",
+  appId: "1:398140502966:web:8666a26281a6410ba7c717"
 };
-
 firebase.initializeApp(config);
